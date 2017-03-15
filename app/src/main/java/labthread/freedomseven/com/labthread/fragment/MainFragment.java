a5e7105f31850b2ee02e369b42ba991f04d16ecf
nut077 <nuttapon.tavee@gmail.com>
2017-03-15 17:56:31 +0700

    thread

--- app/src/main/java/labthread/freedomseven/com/labthread/fragment/MainFragment.java
+++ app/src/main/java/labthread/freedomseven/com/labthread/fragment/MainFragment.java
@@ -1,6 +1,9 @@
 package labthread.freedomseven.com.labthread.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +17,10 @@ import labthread.freedomseven.com.labthread.databinding.FragmentMainBinding;
 public class MainFragment extends Fragment {
 
     FragmentMainBinding binding;
+    private int counter = 0;
+    Thread thread;
+    Handler handler;
+
     public MainFragment() {
         super();
     }
@@ -49,9 +56,71 @@ public class MainFragment extends Fragment {
 
     @SuppressWarnings("UnusedParameters")
     private void initInstances(Bundle savedInstanceState) {
-        // Init 'View' instance(s) with rootView.findViewById here
-        // Note: State of variable initialized here could not be saved
-        //       in onSavedInstanceState
+        /*
+        --- thread 1 ---
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    counter++;
+                    try {
+                        thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            binding.tvCounter.setText(String.valueOf(counter));
+                        }
+                    });
+                }
+            }
+        });
+        thread.start();*/
+
+        // --- thread 2 ---
+        /*
+        handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                binding.tvCounter.setText(String.valueOf(msg.arg1));
+            }
+        };
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    counter++;
+                    try {
+                        thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+
+                    Message message = new Message();
+                    message.arg1 = counter;
+                    handler.sendMessage(message);
+                }
+            }
+        });
+        thread.start();
+        */
+
+        // --- thread 3 ---
+        handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                counter++;
+                binding.tvCounter.setText(String.valueOf(counter));
+                if (counter < 100) {
+                    sendEmptyMessageDelayed(0, 1000);
+                }
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 1000);
     }
 
     @Override
@@ -63,5 +132,11 @@ public class MainFragment extends Fragment {
     @SuppressWarnings("UnusedParameters")
     private void onRestoreInstanceState(Bundle savedInstanceState) {
         // Restore Instance (Fragment level's variables) State here
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //thread.interrupt();
     }
 }
